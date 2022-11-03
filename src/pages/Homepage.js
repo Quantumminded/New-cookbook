72729594d17fde23470c24ede9e04184c2e62609
Luca <lukkino31@gmail.com>
2022-11-03 17:38:33 +0100

    fixed img bug in home page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,25 +4,22 @@ import { Link } from 'react-router-dom'
 
 // create home page 
 export default function Homepage() {
-    const { loading, error, data } = useFetch('http://localhost:1337/api/recipes?populate=*')
+    const { loading, error, data } = useFetch('http://localhost:1337/api/recipes?populate=image')
 
-    if (loading) return <p>Loading ...........</p>
+    if (loading) return <p>Loading ...........</p> 
     if (error) return <p>Error 404 !!!!!!!!!!!</p>
 
-    console.log(data.data[0])
+    console.log(data.data[0].attributes.image.data.attributes)
   return (
     <div>
         {data.data.map(recipe => (
             <div key={recipe.id} className="recipe-card">
-              <img src="http://localhost:1337/uploads/apple_galette_25_600x900_af34d53f83.jpg" alt='img'/>
+              <img src={`http://localhost:1337${recipe.attributes.image.data.attributes.url}`} alt='img'/>
 
               <h2 className='recipe-title'>{recipe.attributes.title}</h2>
               <p className='description'>{recipe.attributes.description.substring(0, 200)} ...</p>
 
               <Link to={`/recipe/${recipe.id}`}><h4>Full Recipe</h4></Link>
-              
-              
-              {/* <img src={recipe.data.attributes.image.data.attributes.url} alt='img'/> */}
         </div>
         ))}
     </div>
