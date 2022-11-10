188d2da945e3d5e4560f248d081ca79c44d65391
Luca <lukkino31@gmail.com>
2022-11-10 17:33:24 +0100

    Header styled with bootstrap

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,13 +23,40 @@ export default function Header() {
 
   console.log(data.categories.data[0].id)
   return (
-    <div className="header">
-      <Link className='Home-button' to='/'>The secret book of Ada&Vale recipes</Link>
-        <nav className="categories">
-          {data.categories.data.map(category => (
-              <Link key={category.id} to={`/category/${category.id}`}>{category.attributes.title}</Link>
-          ))}
-        </nav>
-    </div>
+
+    // <div className="header">
+    //   <Link className='navbar-brand' to='/'>The secret book of Ada&Vale recipes</Link>
+    //     <nav className="navbar navbar-dark bg-primary">
+    //       {data.categories.data.map(category => (
+    //           <Link className="nav-link" key={category.id} to={`/category/${category.id}`}>{category.attributes.title}</Link>
+    //       ))}
+    //     </nav>
+    // </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">The secret's Ada&Vale recipes</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <Link class="nav-link" to='/'>Home</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link" to={`/category/${data.categories.data[0].id}`}>{data.categories.data[0].attributes.title}</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link" to={`/category/${data.categories.data[1].id}`}>{data.categories.data[1].attributes.title}</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link" to={`/category/${data.categories.data[2].id}`}>{data.categories.data[2].attributes.title}</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link" to={`/category/${data.categories.data[3].id}`}>{data.categories.data[3].attributes.title}</Link>
+      </li>
+    </ul>
+  </div>
+</nav>
   )
 }
